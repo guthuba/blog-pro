620282d4fcf572d407040b1241d81f5a26afca68
z666888www@163.com <githuba>
2021-01-22 17:00:16 +0800

    今天的首页什么的

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,24 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
+//首页路由配置
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
-
+//登录路由配置
+router.get('/login',function(req,res){
+  res.render('login',{})
+})
+//注册路由配置
+router.get('/zhuce',function(req,res){
+  res.render('zhuce',{})
+})
+//详情页路由配置
+router.get('/details',function(req,res){
+  res.render('details',{})
+})
+//写文章页路由配置
+router.get('/xie',function(req,res){
+  res.render('xie',{})
+})
 module.exports = router;
