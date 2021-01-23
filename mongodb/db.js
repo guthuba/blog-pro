07623c3aef290cf22248cb32ab2f32a004ed86ac
z666888www@163.com <githuba>
2021-01-23 17:06:24 +0800

    成功连接数据库

---
+++ mongodb/db.js
@@ -0,0 +1,12 @@
+// 1.安装
+// 2.导入安装模板
+let mongoose = require('mongoose')//CMD模块化导入语句
+// 创建一个数据库地址
+let dbUrl = 'mongodb://localhost/disco'//disco数据库名
+// 创建连接
+mongoose
+    .connect(dbUrl)
+    .then(()=>console.log('数据库连接成功'))
+    .catch(err=>{console.log('连接失败'+err)})
+
+    module.exports = mongoose//CMD模块化导出语句
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,42 @@
 var express = require('express');
 var router = express.Router();
 
+// 将用户模板导入
+let User = require('../models/user')
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
+//实现用户信息提交,注册事项
+// Response响应
+// Request请求
+router.post('/addUser', (req, res, next)=>{
+  // 通过req.body获取信息
+  console.log(req.body);
+  // res.send('点击注册了')
 
+  let userInfo = {
+    userName:req.body.userName,
+    password:req.body.password,
+    passwordC:req.body.passwordC,
+  }
+//用户验证
+if(userInfo.password !== userInfo.passwordC){
+  // console.log('密码不一致');
+  let error ={
+    status:0,//错误编码
+    stack:''//错误代码
+  }
+  res.render('error',{error,message:'密码不一致'})
+}
+// 页面表单数据，放入模型
+  let userI = new User(userInfo)
+// 保存
+  userI.save((err,result)=>{
+    if(!err){
+      res.send(result)
+    }
+  })
+})
 module.exports = router;
